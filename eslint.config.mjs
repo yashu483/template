788dde758116080cfa2af5ad25726bcfdddc4e65
Yashu Lilhare <182187926+yashu483@users.noreply.github.com>
2025-06-27 11:53:18 +0530

    fix:solved eslint-config-prettier not working

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,23 @@
-import config from "eslint-config-xo";
-import { defineConfig } from "eslint/config";
-import eslintConfigPrettier from "eslint-config-prettier/flat";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettier from 'prettier';
+import prettierConfig from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
 
-export default [defineConfig([config]), eslintConfigPrettier];
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+    { ignores: ['node_modules/', 'dist/', 'build/'] },
+    { files: ['**/*.{js,mjs,cjs,ts}'] },
+    { languageOptions: { globals: globals.node } },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    prettier,
+    prettierConfig,
+    {
+        plugins: { prettier: prettierPlugin },
+        rules: {
+            'prettier/prettier': 'error',
+        },
+    },
+];
