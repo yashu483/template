835382e9ca378cc59e523d6dc58ea381b990c63d
Yashu Lilhare <182187926+yashu483@users.noreply.github.com>
2025-07-04 10:31:49 +0530

    fix : add webp and avif extension bundler

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,40 @@
-import globals from 'globals';
 import pluginJs from '@eslint/js';
+import globals from 'globals';
+import pluginJson from '@eslint/json';
+import markdown from '@eslint/markdown';
+import css from '@eslint/css';
 import tseslint from 'typescript-eslint';
-import prettier from 'prettier';
 import prettierConfig from 'eslint-config-prettier';
 import prettierPlugin from 'eslint-plugin-prettier';
+import { defineConfig } from 'eslint/config';
 
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-    { ignores: ['node_modules/', 'dist/', 'build/'] },
-    { files: ['**/*.{js,mjs,cjs,ts}'] },
-    { languageOptions: { globals: globals.node } },
-    pluginJs.configs.recommended,
-    ...tseslint.configs.recommended,
-    prettier,
-    prettierConfig,
-    {
-        plugins: { prettier: prettierPlugin },
-        rules: {
-            'prettier/prettier': 'error',
-        },
+export default defineConfig([
+  { ignores: ['node_modules/', 'dist/', 'build/'] },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
     },
-];
+  },
+  pluginJs.configs.recommended,
+  {
+    files: ['**/*.{ts,tsx}'],
+    ...tseslint.configs.recommended[0],
+  },
+  pluginJson.configs.recommended,
+  markdown.configs.recommended,
+  {
+    files: ['**/*.css'],
+    plugins: { css },
+    languageOptions: {
+      parser: css.parser,
+    },
+  },
+  prettierConfig,
+  {
+    plugins: { prettier: prettierPlugin },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
+]);
--- webpack.common.js
+++ webpack.common.js
@@ -1,34 +1,33 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
-    app: "./src/index.js",
+    app: './src/index.js',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/template.html",
-      title: "Production",
+      template: './src/template.html',
     }),
   ],
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.html$/i,
-        loader: "html-loader",
+        loader: 'html-loader',
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
+        test: /\.(webp|avif|png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
       },
     ],
   },
